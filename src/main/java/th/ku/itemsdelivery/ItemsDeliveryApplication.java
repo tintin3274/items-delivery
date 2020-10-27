7e0d89c7e10a193df24eb5e37bd7e74408a3d07f
tintin3274 <tintin3274@hotmail.co.th>
2020-10-27 21:33:22 +0700

    initialize project

---
+++ src/main/java/th/ku/itemsdelivery/ItemsDeliveryApplication.java
@@ -0,0 +1,13 @@
+package th.ku.itemsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemsDeliveryApplication.class, args);
+    }
+
+}
