f7353c8146cadf02c253e249fd7ed8c7d8fdcf7d
tintin3274 <tintin3274@hotmail.co.th>
2020-10-28 23:56:16 +0700

    Create Repositories

---
+++ src/main/java/th/ku/itemsdelivery/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package th.ku.itemsdelivery.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import th.ku.itemsdelivery.model.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
---
+++ src/main/java/th/ku/itemsdelivery/repositories/ItemExportRepository.java
@@ -0,0 +1,10 @@
+package th.ku.itemsdelivery.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import th.ku.itemsdelivery.model.ItemExport;
+import th.ku.itemsdelivery.model.ItemExportId;
+
+@Repository
+public interface ItemExportRepository extends JpaRepository<ItemExport, ItemExportId> {
+}
