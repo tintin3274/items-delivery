df98790825971ab1ddcb1063327fe4e83f6c2d9b
piyathat sula <piyathat.s@ku.th>
2020-11-14 00:16:25 +0700

    Update HomeController and HistoryOrderController

--- src/main/java/th/ku/itemsdelivery/controller/HistoryOrderController.java
+++ src/main/java/th/ku/itemsdelivery/controller/HistoryOrderController.java
@@ -5,21 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import th.ku.itemsdelivery.service.OrderRequestService;
 
 @Controller
 @RequestMapping("/history_order")
 public class HistoryOrderController {
-//    private OrderRequestService orderRequestService;
-//
-//    public HistoryOrderController(OrderRequestService orderRequestService){
-//        this.orderRequestService = orderRequestService;
-//    }
-//
-//    @GetMapping
-//    public String getHistoryPage(Model model){
-//        model.addAttribute("allOrders", orderRequestService.getOrders());
-//        return "history_order";
-//    }
+    private OrderRequestService orderRequestService;
 
+    public HistoryOrderController(OrderRequestService orderRequestService){
+        this.orderRequestService = orderRequestService;
+    }
 
+    @GetMapping
+    public String getHistoryPage(Model model){
+        model.addAttribute("allHistoryOrders", orderRequestService.getOrderRequestStatusAll("CANCEL").addAll(orderRequestService.getOrderRequestStatusAll("SUCESS")));
+        return "history_order";
+    }
 }
