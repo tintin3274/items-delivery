a98193f8e30c1067caa457b062250dcc8a5d9e42
Natnicha Khongsoontorn <natnicha.kho@ku.th>
2020-11-14 17:53:27 +0700

    update SelectItemController

--- src/main/java/th/ku/itemsdelivery/controller/SelectItemController.java
+++ src/main/java/th/ku/itemsdelivery/controller/SelectItemController.java
@@ -1,10 +1,41 @@
 package th.ku.itemsdelivery.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import th.ku.itemsdelivery.model.DateTimeAdapter;
+import th.ku.itemsdelivery.model.OrderRequest;
+import th.ku.itemsdelivery.service.OrderRequestService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
+@RequestMapping("/select_item")
 public class SelectItemController {
-    @RequestMapping("/select_item")
+    private OrderRequestService orderRequestService;
+
+    public SelectItemController(OrderRequestService orderRequestService) {
+        this.orderRequestService = orderRequestService;
+    }
+
+    @GetMapping
     public String getSelectPage(){return "select_item";}
+
+    @PostMapping
+    public String select(HttpServletRequest request, Model model){
+        String[] s=request.getParameterValues("duallistbox_demo1[]");
+        System.err.println(Arrays.toString(s));
+
+
+            ArrayList<OrderRequest> currentOrderslist=new ArrayList<>();
+            DateTimeAdapter dateTimeAdapter =new DateTimeAdapter();
+            currentOrderslist.addAll(orderRequestService.getOrderRequestStatusAll("PENDING"));
+            currentOrderslist.addAll(orderRequestService.getOrderRequestStatusAll("PROGRESS"));
+        model.addAttribute("allOrders",currentOrderslist);
+        model.addAttribute("dateTimeAdapter",dateTimeAdapter);
+        return "home";
+    }
 }
