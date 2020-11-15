96aacf9d7db4ed236ef11d8e8fcf5e8d735400f1
Natnicha Khongsoontorn <natnicha.kho@ku.th>
2020-11-15 23:04:14 +0700

    update import_item.html DONE disable refresh button in search_customer.html

--- src/main/java/th/ku/itemsdelivery/controller/ImportItemController.java
+++ src/main/java/th/ku/itemsdelivery/controller/ImportItemController.java
@@ -1,10 +1,23 @@
 package th.ku.itemsdelivery.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import th.ku.itemsdelivery.service.ItemService;
 
 @Controller
+@RequestMapping("/import_item")
 public class ImportItemController {
-    @RequestMapping("/import_item")
-    public String getPage(){return "import_item";}
+    private ItemService itemService;
+
+    public ImportItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public String getImportPage(Model model) {
+        model.addAttribute("allItems",itemService.getItemAll());
+        return "import_item";
+    }
 }
